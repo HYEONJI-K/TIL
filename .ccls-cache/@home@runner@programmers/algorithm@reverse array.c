3b55166885212196c357dd133e6b54e637385853
HYEONJI KIM <61086165+HYEONJI-K@users.noreply.github.com>
2022-10-26 11:18:27 +0000

    Programmers

---
+++ .ccls-cache/@home@runner@programmers/algorithm@reverse array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+  int a[5][5], j = 0, sw = 1, k = 1;
+  for (int i = 0; i < 5; i++) {
+    while (j <= 4 && j >= 0) {
+      a[i][j] = k++;
+      j += sw;
+    }
+    sw *= -1;
+    j += sw;
+    printf("%d ", sw);
+    printf("%d \n", j);
+  }
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      printf("%3d", a[i][j]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
